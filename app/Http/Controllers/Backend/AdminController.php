4fb16d6e98ec03b78a614a2814dd72aa850cf31c
Ali Raza <alirazabinhussain@gmail.com>
2023-12-21 01:26:45 +0500

    Update Password Function

--- app/Http/Controllers/Backend/AdminController.php
+++ app/Http/Controllers/Backend/AdminController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\File;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
 
 
 class AdminController extends Controller
@@ -45,6 +48,56 @@ class AdminController extends Controller
         /** @var \App\Models\User $user **/
         $user->save();
 
+        toastr()->success('Profile has been updated successfully!', 'Congrats');
+
         return redirect()->back();
     }
+
+    public function updatePassword(){
+        return view('admin.password.view');
+
+    }
+    public function storePassword(Request $request){
+        $request->validate([
+            'current_password' => 'required',
+            'password' => [
+                'required',
+                'min:8',
+                'confirmed',
+            ],
+        ]);
+    
+        $user = Auth::user();
+    
+        if (!Hash::check($request->current_password, $user->password)) {
+            toastr()->error('The current password is incorrect.', 'Error');
+            return redirect()->back();
+        }
+    
+        if (Hash::check($request->password, $user->password)) {
+            toastr()->error('New password must be different from the current password.', 'Error');
+            return redirect()->back();
+        }
+
+        if (strlen($request->password) < 8) {
+            toastr()->error('New password must be at least 8 characters long.', 'Error');
+            return redirect()->back();
+        }
+        
+        if ($request->password !== $request->password_confirmation) {
+            toastr()->error('New password and confirm password do not match.', 'Error');
+            return redirect()->back();
+        }
+    
+        $user->password = Hash::make($request->password);
+    
+        /** @var \App\Models\User $user **/
+        $user->save();
+    
+        toastr()->success('Password updated successfully!', 'Congrats');
+        return redirect()->back();
+    }
+    
+    
+    
 }
