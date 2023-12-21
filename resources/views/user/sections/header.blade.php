652f796d54a9323b2b736ec507725f1fbdd35169
Ali Raza <alirazabinhussain@gmail.com>
2023-12-22 00:10:10 +0500

    User Profile Header

--- resources/views/user/sections/header.blade.php
+++ resources/views/user/sections/header.blade.php
@@ -10,15 +10,15 @@
                 <div class="relative inline-block text-gray-200">
                     <!-- <button id="profileButton" class="text-white hover:underline">Profile</button> -->
                     <div class="flex items-center space-x-4" id="profileButton">
-                        <img src="user-profile-picture.jpg" alt="User Profile" class="w-12 h-12 rounded-full cursor-pointer">
+                        <img src="{{ asset(Auth::user()->image) }}" alt="User Profile" class="w-12 h-12 rounded-full cursor-pointer">
                         <div>
                             <h2 class="text-xl font-semibold text-white cursor-pointer">Welcome, {{Auth::user()->name}}!</h2>
                             <p class="text-gray-200 text-sm cursor-pointer">{{Auth::user()->role}}</p>
                         </div>
                     </div>
                     <ul id="profileDropdown" class="hidden mt-2 py-2 w-32 bg-white border rounded-lg shadow-lg absolute right-0" x-cloak>
-                        <li><a href="#" class="block px-4 py-2 text-gray-800 hover:bg-gray-200">My Profile</a></li>
-                        <li><a href="#" class="block px-4 py-2 text-gray-800 hover:bg-gray-200">Settings</a></li>
+                        <li><a href="{{route('user.profile')}}" class="block px-4 py-2 text-gray-800 hover:bg-gray-200">My Profile</a></li>
+                        <li><a href="{{route('user.password.update')}}" class="block px-4 py-2 text-gray-800 hover:bg-gray-200">Change Password</a></li>
                         <li>
                             <form method="POST" action="{{route('logout')}}">
                                 @csrf
