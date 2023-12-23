571588d6573cc9e4c6cc0972003ce41a4e16c3af
Ali Raza <alirazabinhussain@gmail.com>
2023-12-24 01:14:58 +0500

    All Loan Types Controller

---
+++ app/Http/Controllers/Backend/Admin/LoanTypesController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Backend\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoanTypesController extends Controller
+{
+    public function allLoanTypes()
+    {
+        return view('admin.loan_type.all_loan_type');
+    }
+}
