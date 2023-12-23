151f1f83c9bbdbd0db7ef805bdc5654c2443e9c2
Ali Raza <alirazabinhussain@gmail.com>
2023-12-23 23:57:48 +0500

    User Details Fucntion

--- app/Http/Controllers/Backend/Admin/UsersController.php
+++ app/Http/Controllers/Backend/Admin/UsersController.php
@@ -8,10 +8,11 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
-    public function allUsers(){
+    public function allUsers()
+    {
 
-        $users= User::orderBy('id')->get();
-        return view('admin.users.all_users',compact('users'));
+        $users = User::orderBy('id')->get();
+        return view('admin.users.all_users', compact('users'));
     }
 
     public function deleteUser(User $user)
@@ -20,5 +21,13 @@ class UsersController extends Controller
 
         toastr()->success('User deleted successfully', 'Congrats');
         return redirect()->back();
+    }
+    public function userDetail($id)
+    {
+        // Retrieve user details using the $id parameter
+        $user = User::findOrFail($id);
+
+        // Pass the user details to the view
+        return view('admin.users.detail', compact('user'));
     }
 }
