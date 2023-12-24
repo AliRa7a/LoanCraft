8fc1c99d59ba96a1df8d30474dd658172ea0dbe6
Ali Raza <alirazabinhussain@gmail.com>
2023-12-24 17:30:26 +0500

    All Loan Types Function

--- app/Http/Controllers/Backend/Admin/LoanTypesController.php
+++ app/Http/Controllers/Backend/Admin/LoanTypesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Backend\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\LoanTypes;
 use Illuminate\Http\Request;
 
 class LoanTypesController extends Controller
@@ -10,5 +11,18 @@ class LoanTypesController extends Controller
     public function allLoanTypes()
     {
         return view('admin.loan_type.all_loan_type');
+    }
+
+    public function addLoanType(Request $request)
+    {
+        $validateData = $request->validate([
+            'loanType' => 'required',
+        ]);
+        $loanType = new LoanTypes();
+        $loanType->name = $validateData['loanType'];
+
+        $loanType->save();
+        toastr()->success('Loan type added successfully', 'Congrats');
+        return redirect()->back();
     }
 }
