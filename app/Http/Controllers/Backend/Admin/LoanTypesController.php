39cf9e5173d86df9c60d0a4327c2edfa52ffd095
Ali Raza <alirazabinhussain@gmail.com>
2023-12-25 21:41:59 +0500

    Delete, Fetch and update loan fucntions

--- app/Http/Controllers/Backend/Admin/LoanTypesController.php
+++ app/Http/Controllers/Backend/Admin/LoanTypesController.php
@@ -10,7 +10,8 @@ class LoanTypesController extends Controller
 {
     public function allLoanTypes()
     {
-        return view('admin.loan_type.all_loan_type');
+        $loans = LoanTypes::orderBy('id')->get();
+        return view('admin.loan_type.all_loan_type', compact('loans'));
     }
 
     public function addLoanType(Request $request)
@@ -24,5 +25,31 @@ class LoanTypesController extends Controller
         $loanType->save();
         toastr()->success('Loan type added successfully', 'Congrats');
         return redirect()->back();
+    }
+
+    public function deleteLoanType(LoanTypes $loan_type)
+    {
+        $loan_type->delete();
+        toastr()->success('Loan type deleted successfully', 'Congrats');
+        return redirect()->back();
+    }
+
+    public function editLoanType($id)
+    {
+        $loanType = LoanTypes::findOrFail($id);
+        return view('admin.loan_type.edit_loan_type', compact('loanType'));
+    }
+
+    public function updateLoanType(Request $request, $id)
+    {
+        $loanType = LoanTypes::findOrFail($id);
+        $validateData = $request->validate([
+            'loanType' => 'required',
+        ]);
+        $loanType->update([
+            'name' => $validateData['loanType'],
+        ]);
+        toastr()->success('Loan type updated successfully', 'Congrats');
+        return redirect()->route('admin.all.loan.types');
     }
 }
