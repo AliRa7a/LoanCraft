7b97faf0c3006abbd49532dd1be8f65ca6096f6e
Ali Raza <alirazabinhussain@gmail.com>
2023-12-25 21:40:31 +0500

    Fetch loan data

--- resources/views/admin/loan_type/all_loan_type.blade.php
+++ resources/views/admin/loan_type/all_loan_type.blade.php
@@ -4,7 +4,7 @@
 <div class="flex p-6 mx-auto  ">
     <div class="w-1/2 bg-white shadow-md rounded-lg p-4">
         <h2 class="text-2xl font-semibold mb-4">Loan Management</h2>
-        <form method="post" action="{{route('admin.add.loan.type')}}">
+        <form method="post" action="{{route('add.loan.type')}}">
             @csrf
             <div class="mb-4">
                 <label for="loanType" class="block text-gray-700 font-medium">Loan Type</label>
@@ -18,29 +18,27 @@
         <table class="table-auto w-full">
             <thead>
                 <tr>
-                    <th class="px-4 py-2">Serial Number</th>
+                    <th class="px-4 py-2">Sr#</th>
                     <th class="px-4 py-2">Loan Type</th>
                     <th class="px-4 py-2">Actions</th>
                 </tr>
             </thead>
             <tbody>
+                @foreach($loans as $index => $loan)
                 <tr>
-                    <td class="px-4 py-2">1</td>
-                    <td class="px-4 py-2">Personal Loan</td>
+                    <td class="px-4 py-2">{{ $index + 1 }}</td>
+                    <td class="px-4 py-2">{{ $loan->name }}</td>
                     <td class="px-4 py-2">
-                        <button class="bg-blue-500 text-white py-1 px-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-200 focus:ring-opacity-50">Update</button>
-                        <button class="bg-red-500 text-white py-1 px-2 rounded-md hover:bg-red-600 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50">Delete</button>
+                        <a href="{{route('edit.loan.type', $loan->id)}}" class="bg-blue-500 text-white py-1 px-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-200 focus:ring-opacity-50">Update</a>
+                        <button type="submit" onclick="deleteLoanType('{{$loan->id}}')" class="bg-red-500 text-white py-1 px-2 rounded-md hover:bg-red-600 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50">Delete</button>
+                        <form action="{{ route('delete.loan.type', $loan->id) }}" method="POST" id="delete-loan{{$loan->id}}">
+                            @csrf
+                            @method('DELETE')
+
+                        </form>
                     </td>
                 </tr>
-                <tr>
-                    <td class="px-4 py-2">2</td>
-                    <td class="px-4 py-2">Home Loan</td>
-                    <td class="px-4 py-2">
-                        <button class="bg-blue-500 text-white py-1 px-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-200 focus:ring-opacity-50">Update</button>
-                        <button class="bg-red-500 text-white py-1 px-2 rounded-md hover:bg-red-600 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50">Delete</button>
-                    </td>
-                </tr>
-                <!-- Add more records as needed -->
+                @endforeach
             </tbody>
         </table>
     </div>
