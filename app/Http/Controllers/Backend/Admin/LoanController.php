248c0660147e9cb9d1f7d952cab1e51197faf81a
Ali Raza <alirazabinhussain@gmail.com>
2023-12-26 23:58:26 +0500

    all loan application controller

--- app/Http/Controllers/Backend/Admin/LoanController.php
+++ app/Http/Controllers/Backend/Admin/LoanController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\Backend\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\LoanApplication;
 use Illuminate\Http\Request;
 
 class LoanController extends Controller
 {
-    //
+    public function allLoanApplications()
+    {
+        $loan = LoanApplication::latest()->get();
+        return view('admin.loan_application.all', compact('loan'));
+    }
 }
