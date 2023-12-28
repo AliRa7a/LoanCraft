c40fe426d2b54a50d00dd5189cfb6e11cf2d94b8
Ali Raza <alirazabinhussain@gmail.com>
2023-12-29 01:17:45 +0500

    Implemented user loan application handling in loanStore method

--- app/Http/Controllers/Backend/Admin/LoanController.php
+++ app/Http/Controllers/Backend/Admin/LoanController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers\Backend\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\LoanApplication;
 use App\Models\LoanTypes;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 
 class LoanController extends Controller
 {
@@ -19,5 +22,31 @@ class LoanController extends Controller
     {
         $loan_types = LoanTypes::all();
         return view('user.loan.application', compact('loan_types'));
+    }
+
+    public function loanStore(Request $request)
+    {
+        $id = Auth::user()->id;
+        $data = User::find($id);
+
+        $today = Carbon::now();
+        $formattedDate = $today->format('Y-m-d');
+
+        LoanApplication::insert([
+            'name' => $data->name,
+            'email' => $data->email,
+            'amount' => $request->amount,
+            'bank' => $request->bank,
+            'account_number' => $request->account_no,
+            'loan_type' => $request->loan_type,
+            'installment_count' => $request->installment_count,
+            'installment_amount' => $request->installment_amount,
+            'amount_payable' => $request->amount_payable,
+            'date_applied' => $formattedDate,
+            'status' => 'not_approved',
+
+        ]);
+        toastr()->success('Loan applied successfully', 'Congrats');
+        return redirect()->back();
     }
 }
