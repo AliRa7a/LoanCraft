3826b8af7e6542e9f5ba3d53a31cd19d8ad1e59b
Ali Raza <alirazabinhussain@gmail.com>
2023-12-31 22:08:46 +0500

    Add Comments to LoanApplication Model for Improved Documentation

--- app/Models/LoanApplication.php
+++ app/Models/LoanApplication.php
@@ -8,8 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class LoanApplication extends Model
 {
     use HasFactory;
+
+    // Guarded attributes to prevent mass assignment
     protected $guarded = [];
 
+    /**
+     * Define a many-to-one relationship with LoanTypes model.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function loan_type()
     {
         return $this->belongsTo(LoanTypes::class);
