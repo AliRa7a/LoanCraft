8f74afe1a54940ae645630fcb19cfcf7b9f45bae
Ali Raza <alirazabinhussain@gmail.com>
2023-12-31 22:05:22 +0500

    Add Comments to UsersController for Improved Documentation

--- app/Http/Controllers/Backend/Admin/UsersController.php
+++ app/Http/Controllers/Backend/Admin/UsersController.php
@@ -8,13 +8,23 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
+    /**
+     * Display all users.
+     *
+     * @return \Illuminate\View\View
+     */
     public function allUsers()
     {
-
         $users = User::orderBy('id')->get();
         return view('admin.users.all_users', compact('users'));
     }
 
+    /**
+     * Delete a user.
+     *
+     * @param \App\Models\User $user
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function deleteUser(User $user)
     {
         $user->delete();
@@ -22,16 +32,31 @@ class UsersController extends Controller
         toastr()->success('User deleted successfully', 'Congrats');
         return redirect()->back();
     }
+
+    /**
+     * Display details of a specific user.
+     *
+     * @param int $id
+     * @return \Illuminate\View\View
+     */
     public function userDetail($id)
     {
         $user = User::findOrFail($id);
         return view('admin.users.detail', compact('user'));
     }
 
+    /**
+     * Update the role of a user.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function updateRole(Request $request, $id)
     {
         $user = User::findOrFail($id);
 
+        // Update the user role based on user input
         $user->role = $request->has('role') ? 'admin' : 'user';
         $user->save();
 
@@ -39,10 +64,18 @@ class UsersController extends Controller
         return redirect()->back();
     }
 
+    /**
+     * Update the status of a user.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function updateStatus(Request $request, $id)
     {
         $user = User::find($id);
 
+        // Update the user status based on user input
         $user->status = $request->has('status') ? 'active' : 'inactive';
         $user->save();
 
